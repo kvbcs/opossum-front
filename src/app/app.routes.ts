51bc739cddabaf5e18f30254633df47a66bb00a1
Kyllian <kyllianvibancos2@gmail.com>
2025-07-15 15:01:07 +0200

    chore: style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './PAGES/home/home.component';
+import { RegisterComponent } from './PAGES/register/register.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Home Page',
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'Register Page',
   },
 ];
