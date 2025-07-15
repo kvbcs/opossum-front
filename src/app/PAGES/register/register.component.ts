14a047397525732e4e20d5c5e99bfc3e817d9486
Kyllian <kyllianvibancos2@gmail.com>
2025-07-15 16:30:28 +0200

    chore: style

--- src/app/PAGES/register/register.component.ts
+++ src/app/PAGES/register/register.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
-export class RegisterComponent {
-
-}
+export class RegisterComponent {}
