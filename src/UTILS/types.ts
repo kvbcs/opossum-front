7fc2e9ef586d7123dd400d0913e37fce9c36e30f
Kyllian <kyllianvibancos2@gmail.com>
2025-07-17 09:54:34 +0200

    feat: types added

---
+++ src/UTILS/types.ts
@@ -0,0 +1,46 @@
+export type Auth = {
+  firstName?: string;
+  lastName?: string;
+  email: string;
+  password: string;
+  gdprAccepted: boolean;
+};
+
+export enum Role {
+  ADMIN = 'Admin',
+  USER = 'User',
+}
+export type User = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  isVerified: boolean;
+  isBlocked: boolean;
+  gdprAcceptedAt: string;
+  role: Role;
+  createdAt: string;
+};
+
+export enum Type {
+  LOST = 'Lost',
+  FOUND = 'Found',
+}
+
+export enum Status {
+  PENDING = 'Pending',
+  RETRIEVED = 'Retrieved',
+  NO_RESPONSE = 'No response',
+}
+
+export type Listing = {
+  title: string;
+  description: string;
+  photo: string;
+  localization: string;
+  eventDate: string;
+  isArchived: boolean;
+  type: Type;
+  status: Status;
+  user: User;
+};
